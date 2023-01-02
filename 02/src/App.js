e4579ad1e7d11dfd0d5a4e952119a583adce8e88
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2023-01-02 17:15:11 -0500

    add new post feature

--- 02/src/App.js
+++ 02/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
 import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import './App.css';
 
 function App() {
   return (
     <main className="App">
       <PostsList />
+      <AddPostForm />
     </main>
   );
 }
